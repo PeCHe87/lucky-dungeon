46c934f3895bb1e12206b8e0cac11875b220853d
PeCHe87 <mauriciojperez@hotmail.com>
2026-09-05 17:27:48 +0200

    - UI: game level can be restarted from Game Over screen

--- Assets/_project/_scripts/gameplay/ui/screens/GameOverScreen.cs
+++ Assets/_project/_scripts/gameplay/ui/screens/GameOverScreen.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// Game-over overlay shown when <see cref="GameEvents.PlayerDied"/> fires.
@@ -6,8 +8,42 @@ using UnityEngine;
 public sealed class GameOverScreen : BaseScreen
 {
     [SerializeField, Min(0f)] float showDelaySeconds = 1f;
+    [SerializeField] Button retryButton;
+
+    Transform _content;
 
     public override ScreenId Id => ScreenId.GameOver;
 
     public override float ShowDelaySeconds => showDelaySeconds;
+
+    void Awake()
+    {
+        _content = transform.Find("content");
+
+        if (retryButton == null)
+            retryButton = GetComponentInChildren<Button>(true);
+
+        if (retryButton != null)
+            retryButton.onClick.AddListener(RestartScene);
+    }
+
+    void OnDestroy()
+    {
+        if (retryButton != null)
+            retryButton.onClick.RemoveListener(RestartScene);
+    }
+
+    protected override void OnShown()
+    {
+        if (_content != null)
+            _content.gameObject.SetActive(true);
+    }
+
+    void RestartScene()
+    {
+        if (retryButton != null)
+            retryButton.interactable = false;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
